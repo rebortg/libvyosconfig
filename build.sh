9ab5ae5dcfcc28db4ac8cd352515833ece099ca0
Robert Göhler <github@ghlr.de>
2024-12-19 13:39:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/rebortg/vyos1x-config.git#5fa988a -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/rebortg/vyos1x-config.git#a84cca2 -y'
 sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#5ae7f4150091efb66c6f9946df81adde3ddc6ace -y'
 
 eval `opam config env`
